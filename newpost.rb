4078509fa72d092c8636a5615fe0b9eac653c345
Johnathan Lyman <jelyman@gmail.com>
2015-12-07 20:11:51 -0800

    New blog post

--- newpost.rb
+++ newpost.rb
@@ -1,36 +1,48 @@
 # Generate a new jekyll blog post using prompts then place it in the _posts folder.
 
+require 'active_support/inflector'
+
 puts 'Enter the post title:'
 title = gets.chomp
+title.titleize
 
-puts 'Enter the date (YYYY-MM-DD) or leave blank for current:'
-date = gets.chomp
+year = Time.now.strftime('%Y')
+month = Time.now.strftime('%m')
+day = Time.now.strftime('%d')
 
-date = Time.now.strftime('%Y-%m-%d') unless date != ""
+hyphen_date = "#{year.to_s}-#{month.to_s}-#{day.to_s}"
 time = Time.now.strftime('%H:%M')
 
 puts 'Enter the category:'
 category = gets.chomp
 
-dirty_slug = title.split(' ')
+sanitized_title = title.downcase.gsub(/[^a-z0-9\s]/i, '')
+
+dirty_slug = sanitized_title.split(' ')
 clean_slug = []
+
 dirty_slug.each do |s|
-  clean_slug << s.downcase
+  clean_slug << s
   clean_slug << '-'
 end
 
 clean_slug.pop
+
 final_slug = clean_slug.join('')
 
-the_post_permalink = "/#{date}/#{final_slug}/"
+the_post_permalink = "/#{year}/#{month}/#{day}/#{final_slug}/"
 
-the_post_file_name = date.to_s + '-' + final_slug.to_s + '.md'
+# Create the _posts directory
+
+system('mkdir', '_posts') unless File.exists?('_posts')
+
+the_post_file_name = hyphen_date + '-' + final_slug.to_s + '.md'
 the_post_file = File.new("_posts/#{the_post_file_name}", 'w')
 the_post_file.puts('---')
 the_post_file.puts("title: \"#{title}\"")
 the_post_file.puts('author: Johnathan Lyman')
 the_post_file.puts('layout: post')
-the_post_file.puts("date: #{date} #{time}")
+the_post_file.puts("date: #{hyphen_date} #{time}")
 the_post_file.puts("permalink: #{the_post_permalink}")
 the_post_file.puts('categories:')
 the_post_file.puts("    - #{category}")
